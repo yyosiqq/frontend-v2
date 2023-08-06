c137168060187ad34e2073ca68a0992a0dd6e884
yyosiqq <141414221+yyosiqq@users.noreply.github.com>
2023-08-06 04:19:24 +0000

    Pending changes exported from your codespace

---
+++ src/lib/config/avalanche/yyosiqq/yyosiqq-pools.ts
@@ -0,0 +1,13 @@
+const QUERY_KEYS: {
+    Pools: {
+        All: (networkId: Ref<Network>, tokens: Ref<string[]>, poolsSortField: Ref<string> | undefined, poolIds: Ref<string[]> | undefined, poolAddresses: Ref<string[]> | undefined) => (string | {
+            networkId: Ref<Network>;
+            tokens: Ref<string[]>;
+            poolsSortField: Ref<string> | undefined;
+            poolIds: Ref<0x3f1a2c4a3a751f6626bd90ef16e104f0772d4d6b00020000000000000000001b> | undefined;
+            poolAddresses: Ref<0x3f1A2C4a3a751f6626bD90eF16e104f0772d4d6B> | undefined;
+        })[];
+
+const QUERY_KEYS: {
+    pools (poolAddresses:<0x3f1A2C4a3a751f6626bD90eF16e104f0772d4d6B> | undefined; }
+   
